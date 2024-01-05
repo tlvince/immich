d30781c868085fbcb9228f923fab2cbc48f11056
mertalev <101130780+mertalev@users.noreply.github.com>
2024-01-04 20:55:35 -0500

    wip dbscan

--- server/src/domain/repositories/smart-info.repository.ts
+++ server/src/domain/repositories/smart-info.repository.ts
@@ -7,12 +7,23 @@ export type Embedding = number[];
 export interface EmbeddingSearch {
   userIds: string[];
   embedding: Embedding;
-  numResults: number;
+  numResults?: number;
 }
 
 export interface FaceEmbeddingSearch extends EmbeddingSearch {
   maxDistance?: number;
   noPerson?: boolean;
+  hasPerson?: boolean;
+}
+
+export interface FaceEmbeddingDensitySearch {
+  k: number;
+  maxDistance: number;
+}
+
+export interface FaceEmbeddingDensityResult {
+  faceId: string;
+  density: number;
 }
 
 export interface FaceSearchResult {
@@ -24,5 +35,6 @@ export interface ISmartInfoRepository {
   init(modelName: string): Promise<void>;
   searchCLIP(search: EmbeddingSearch): Promise<AssetEntity[]>;
   searchFaces(search: FaceEmbeddingSearch): Promise<FaceSearchResult[]>;
+  getFaceDensities(search: FaceEmbeddingDensitySearch): Promise<FaceEmbeddingDensityResult[]>;
   upsert(smartInfo: Partial<SmartInfoEntity>, embedding?: Embedding): Promise<void>;
 }
