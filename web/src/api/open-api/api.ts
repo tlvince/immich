4a967b268d2ac2a92b8532d1ff4041a026c56c4b
mertalev <101130780+mertalev@users.noreply.github.com>
2024-01-11 22:15:38 -0500

    update api

--- web/src/api/open-api/api.ts
+++ web/src/api/open-api/api.ts
@@ -3774,12 +3774,6 @@ export interface SystemConfigJobDto {
      * @type {JobSettingsDto}
      * @memberof SystemConfigJobDto
      */
-    'facialRecognition': JobSettingsDto;
-    /**
-     * 
-     * @type {JobSettingsDto}
-     * @memberof SystemConfigJobDto
-     */
     'library': JobSettingsDto;
     /**
      * 
